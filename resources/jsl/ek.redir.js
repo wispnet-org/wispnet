6ad82397e57b593a8c7ad7875b91d392ab66a28e
wispgitgecko9 <mailgecko9@gmail.com>
2026-07-02 15:32:19 -0500

    Update to Iteration Three (i3)
    WISPNET Web Iteration Three (i3) update notes:
    
    - Reworked web homepage to UDT (Uniform Design Template.)
    - Updated TDP index.html.
    - Updated and added CSS files.
    - Added QR code generator project.
    - Added preliminary game project "xgn".
    - Moved the JavaScript resources from resources/js to resources/jsl.

---
+++ resources/jsl/ek.redir.js
@@ -0,0 +1,27 @@
+const targetUrl = "https://eitkek.com";
+let timeLeft = 7;
+let cancelled = false;
+
+const countdownEl = document.getElementById("countdown");
+
+const timer = setInterval(() => {
+  if (cancelled) {
+    clearInterval(timer);
+    countdownEl.textContent = "cancelled";
+    return;
+  }
+
+  timeLeft--;
+  countdownEl.textContent = timeLeft;
+
+  if (timeLeft <= 0) {
+    clearInterval(timer);
+    window.location.href = targetUrl;
+  }
+}, 1000);
+
+    function cancelRedirect() {
+      cancelled = true;
+    }
+
+    
